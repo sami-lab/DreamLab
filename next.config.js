898f797c5ffd1a69d8e440a1527319d255af4fd8
Sami <s.m.sami125@gmail.com>
2021-02-16 13:09:29 +0500

    Added project api route

--- next.config.js
+++ next.config.js
@@ -1,12 +1,31 @@
 const withPlugins = require('next-compose-plugins');
 const optimizedImages = require('next-optimized-images');
- 
 
 module.exports = withPlugins([
-    [optimizedImages, {
+  [
+    optimizedImages,
+    {
       /* config for next-optimized-images */
-    }],
-   
-    // your other plugins here
-   
-  ]);
+    },
+  ],
+
+  // your other plugins here
+  {
+    api: {
+      bodyParser: {
+        sizeLimit: '10mb',
+      },
+    },
+  },
+]);
+
+// module.exports = optimizedImages({
+//   /* config for next-optimized-images */
+
+//   // your config for other plugins or the general next.js here...
+//   api: {
+//     bodyParser: {
+//       sizeLimit: '15mb',
+//     },
+//   },
+// });
